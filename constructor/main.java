f17477288e684f931ff8fa59ccd382e777eaadb1
emphi73 <emesfin73@yahoo.com>
2022-12-15 19:11:13 -0600

    pizza task

---
+++ constructor/main.java
@@ -0,0 +1,9 @@
+package constructor;
+
+public class main {
+    public static void main(String[] args) {
+        pizza order1 = new pizza('m',2,1);
+        pizza order2 = new pizza('l', 2,2);
+        pizza order3 = new pizza('s', 2,3);
+    }
+}

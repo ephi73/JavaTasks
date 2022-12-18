3f6866e60038e886539693551ec31691114c5564
emphi73 <emesfin73@yahoo.com>
2022-12-18 13:53:10 -0600

    carpet task

---
+++ constructor/Offer/main.java
@@ -0,0 +1,10 @@
+package constructor.Offer;
+
+public class main {
+    public static void main(String[] args) {
+        Offer newOffer = new Offer("Chicago","Software Engineer", 10000,true,true,
+                true, true);
+        System.out.println(newOffer);
+    }
+
+}
